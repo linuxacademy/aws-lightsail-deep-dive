caf69a670fcacd45e0cabe3afa11e9c318346785
Adrian Cantrill <adrian@cantrill.io>
2018-12-30 11:50:36 +1000

    fixes

--- Scenario3/learning_activity_1/launchscript.sh
+++ Scenario3/learning_activity_1/launchscript.sh
@@ -3,7 +3,8 @@ sudo /opt/bitnami/ctlscript.sh stop apache
 sudo mv /opt/bitnami/apache2/scripts/ctl.sh /opt/bitnami/apache2/scripts/ctl.sh.disabled
 
 cd /home/bitnami
-sudo git clone https://github.com/linuxacademy/aws-lightsail-deep-dive/Scenario3/todo-mean
+sudo git clone https://github.com/linuxacademy/aws-lightsail-deep-dive
+mv ./aws-lightsail-deep-dive/Scenario3/todo-mean .
 cd /home/bitnami/todo-mean
 sudo npm install --production
 
